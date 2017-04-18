8b0cbbae3d392dcc563f29a072be1e6e25546dcb
bharathc <bharathc@gmail.com>
2017-04-18 16:37:40 -0400

    Storing SongList titles, genres, years, and artists  in array lists

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -6,6 +6,7 @@ package prj5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -19,12 +20,28 @@ public class Input {
      */
    public static void main (String args[]) throws FileNotFoundException{
        //Scanner scanSurDat = new Scanner(new File("MusicSurveyData.csv"));
+       
+       //scan the file SongList and get relevant data
        Scanner scanSongLis = new Scanner(new File("SongList.csv"));
-       //int indexLine = 1;
-       scanSongLis.useDelimiter(",");
+       ArrayList<String> songTitleList = new ArrayList<String>();
+       ArrayList<String> songArtistList = new ArrayList<String>();
+       ArrayList<Integer> songYearList = new ArrayList<Integer>();
+       ArrayList<String> songGenreList = new ArrayList<String>();
+       //skip csv title line
+       scanSongLis.nextLine();
        while(scanSongLis.hasNextLine()) {
-           System.out.print(scanSongLis.next()+",   ");
+           //curLine array of the current line
+           String[] curLine = scanSongLis.nextLine().split(",");
+           songTitleList.add(curLine[0]);
+           songArtistList.add(curLine[1]);
+           songYearList.add(Integer.parseInt(curLine[2]));
+           songGenreList.add(curLine[3]);
        }
        scanSongLis.close();
-   }   
+       System.out.println(songTitleList.toString());
+       System.out.println(songArtistList.toString());
+       System.out.println(songYearList.toString());
+       System.out.println(songGenreList.toString());
+   }
+
 }
