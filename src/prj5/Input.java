e303d5e9d19596aabd8dec4413232d38d1f9c35d
bharathc <bharathc@gmail.com>
2017-04-18 16:00:29 -0400

    Added SongList.csv to project folder

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -13,5 +13,18 @@ import java.util.Scanner;
  * @version 04.17.17
  */
 public class Input {
-   
+    /**
+     * main method
+     * @param args arguments passed
+     */
+   public static void main (String args[]) throws FileNotFoundException{
+       //Scanner scanSurDat = new Scanner(new File("MusicSurveyData.csv"));
+       Scanner scanSongLis = new Scanner(new File("SongList.csv"));
+       //int indexLine = 1;
+       scanSongLis.useDelimiter(",");
+       while(scanSongLis.hasNextLine()) {
+           System.out.print(scanSongLis.next()+",   ");
+       }
+       scanSongLis.close();
+   }   
 }
