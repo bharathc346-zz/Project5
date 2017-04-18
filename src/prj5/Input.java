5d62a2132f97c75d446462a6d298f10b50a0887c
bharathc <bharathc@gmail.com>
2017-04-17 21:23:04 -0400

    setup

---
+++ src/prj5/Input.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package prj5;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author bharathchintagunta
+ * @version 04.17.17
+ */
+public class Input {
+   
+}
